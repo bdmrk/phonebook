dfc1923827a0480b06ecc13397b3c2b24a951c70
Mahabubur Rahman Kausar <hostkausar@gmail.com>
2019-05-17 03:55:56 +0600

    home and about page is created with vue router

---
+++ resources/js/components/Myfooter.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      <strong>VueJs Phonebook</strong> by <a href="https://kausar.info">Kausar
+      </a>.
+      
+    </p>
+  </div>
+</footer>
+</template>
---
+++ resources/js/components/Myheader.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://xendsoft.com">
+      <img src="images/xend.png" alt="Kausar" width="150" height="70">
+    </a>
+
+    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu is-active">
+    <div class="navbar-start">
+
+    <router-link to="/home" class="navbar-item">Home</router-link>
+    <router-link to="/about" class="navbar-item">About</router-link>
+
+    </div>
+  </div>
+</nav>
+</template>
